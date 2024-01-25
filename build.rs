f73e562a6dea4152e7fe9a93192137d46583d076
GrapeJuiceSoda <kendallcarmel@gmail.com>
2024-01-25 14:54:14 -0800

    added large constant enum values that are generated at build time

--- build.rs
+++
@@ -1,5 +0,0 @@
-use build_const::build_const;
-
-fn main() {
-    let mut consts = include!(concat!(std::env::var("OUT_DIR"), concat!("/", $mod_name)));
-}
--- src/helper.rs
+++ src/helper.rs
@@ -1,13 +1,25 @@
 use core::arch::asm;
 use core::sync::atomic::{compiler_fence, Ordering};
 
-/// DSB with compiler fence.
+/// DSB(Data Synchronization Barrier) with compiler fence.
 #[inline(always)]
 pub(crate) fn dsb() {
     compiler_fence(Ordering::SeqCst);
 
     // SAFETY: "dsb" is always safe.
     unsafe { asm!("dsb") }
+
+    compiler_fence(Ordering::SeqCst);
+}
+
+/// ISB(Instruction Synchronization Barrier) with compiler fence.
+#[inline(always)]
+pub(crate) fn isb() {
+    // no re-ordering of reads and writes across this point is allowed
+    compiler_fence(Ordering::SeqCst);
+
+    // SAFETY: "isb" is always safe.
+    unsafe { asm!("isb") }
 
     compiler_fence(Ordering::SeqCst);
 }
